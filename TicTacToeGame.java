380025c5cd0f2634b91748593b8d07eac98b168d
Sagar <sagarrashinkar2@gmail.com>
2020-09-30 16:12:17 +0530

    ADDED: user can select his letter

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class TicTacToeGame{
 
+	char userSymbol;
+	char cpuSymbol;
+
+	Scanner sc = new Scanner(System.in);
 
 	public void printBoard(char[][] board){
 	
@@ -12,8 +18,36 @@ class TicTacToeGame{
 			System.out.println();
 		}
 	}
+
+	public void whoStartMatch(){
 	
+		int toss = (int)(Math.random()*2);
+		if(toss == 0)
+			System.out.println("User starts first");
+		else
+			System.out.println("CPU starts first");
+	}
+
+	public void chooseLetter(){
+	
+		System.out.println("enter your symbol(X/O):");
+		userSymbol = sc.next().charAt(0);
+		if(userSymbol == 'X'){
+		
+			cpuSymbol = 'O';
+		}
+		else if(userSymbol == 'O'){
+		
+			cpuSymbol = 'X';
+		}
+		else{
+		
+			System.out.println("please enter a valid symbol from above!!!");
+		}
+	}
+
 	public static void main(String[] args){
+		
 		char[][] board = {{' ', '|', ' ', '|', ' '},
                		{'-', '+', '-', '+', '-'},
                		{' ', '|', ' ', '|', ' '},
@@ -22,5 +56,12 @@ class TicTacToeGame{
 		
 		TicTacToeGame obj = new TicTacToeGame();
 		obj.printBoard(board);
+		
+		obj.whoStartMatch();
+
+		obj.chooseLetter();
+		System.out.println("User symbol is: "+obj.userSymbol);
+		System.out.println("Cpu symbol is: "+obj.cpuSymbol);
+	
 	}
 }
